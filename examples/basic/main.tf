e255aab363606cb5e8f22580847d75d022f9f9e9
tomoki <tomoki171923@gmail.com>
2023-01-22 15:44:10 +0900

    update README & example

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,6 +1,9 @@
 provider "aws" {
   region = "ap-northeast-1"
 }
+data "aws_sns_topic" "apigateway_alarm" {
+  name = "apigateway_alarm"
+}
 
 module "api_key" {
   source   = "../../"
@@ -13,7 +16,7 @@ module "api_key" {
     },
     {
       name       = "client-a-sample-pro-key"
-      usage_plan = "basic"
+      usage_plan = "flex"
     }
   ]
   usage_plans = [
@@ -27,6 +30,15 @@ module "api_key" {
       stages       = ["dev", "st"],
     },
     {
+      name         = "basic",
+      description  = "API Production Usage Plan.",
+      burst_limit  = 100,
+      rate_limit   = 200,
+      quota_limit  = 5000,
+      quota_period = "DAY",
+      stages       = ["pro"],
+    },
+    {
       name         = "flex",
       description  = "API Production Usage Plan.",
       burst_limit  = 300,
@@ -35,5 +47,18 @@ module "api_key" {
       quota_period = "DAY",
       stages       = ["pro"],
     },
+  ]
+  cloudwatch_metric_alarms = [
+    {
+      stage_name    = "pro"
+      label         = "call_limit"
+      description   = "[production] sample API CALL count exceeded 80% of the daily limit."
+      alarm_actions = [data.aws_sns_topic.apigateway_alarm.arn]
+      ok_actions    = [data.aws_sns_topic.apigateway_alarm.arn]
+      metric_name   = "Count"
+      period        = 86400 // 1 day
+      statistic     = "Sum"
+      threshold     = 15000 * 0.8
+    }
   ]
 }
--- output.tf
+++ output.tf
@@ -10,6 +10,6 @@ output "usage_plan_key" {
   value = aws_api_gateway_usage_plan_key.this
 }
 
-output "name" {
+output "cloudwatch_metric_alarm" {
   value = module.cloudwatch_metric_alarm
 }
